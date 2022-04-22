a81388e3eb62a9da4121d2c0138b6f500dface2b
Jones-bass <jonesbass.tb@gmail.com>
2022-04-22 14:12:09 -0300

    Func Virtualized

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { List, ListRowRenderer } from "react-virtualized";
 import { ProducItem } from "./ProducItem";
 
 interface SearchResultsProps {
@@ -7,24 +8,38 @@ interface SearchResultsProps {
     price: number;
     priceFormatted: number;
     title: string;
-  }>
+  }>;
   onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps) {
+export function SearchResults({
+  totalPrice,
+  results,
+  onAddToWishList,
+}: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProducItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
 
   return (
     <div>
       <h1>{totalPrice}</h1>
 
-      {results.map(product => (
-
-        <ProducItem
-          key={product.id}
-          product={product}
-          onAddToWishList={onAddToWishList} />
-      )
-    )}
+      <List
+        height={300}
+        rowHeight={25}
+        width={900}
+        overscanRowCount={10}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
-  )
+  );
 }
